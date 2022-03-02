1bdc44475d7b68e46a0c032affeb652f009770eb
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2022-03-02 03:56:32 +0300

    Change main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from face_processing import landmarks
 import training.dataset as ds
 
 
-# landmarks.track_face()
-ds.generate('files/mini_dataset.pkl', 5)
+landmarks.track_face()
+# ds.generate('files/mini_dataset.pkl', 5)
