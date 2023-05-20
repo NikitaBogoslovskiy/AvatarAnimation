f531e954ff514fa703d50fe95669515030eac4a7
NikitaBogoslovskiy <nbog@sfedu.ru>
2023-05-21 00:36:10 +0300

    feat: change loss function and train video model

--- FLAME/utils.py
+++ FLAME/utils.py
@@ -3,7 +3,7 @@ import torch
 import pickle
 
 
-class Masks:
+class UpperMasks:
     def __init__(self,
                  forehead,
                  eye_region,
@@ -11,6 +11,27 @@ class Masks:
         self.forehead = forehead
         self.eye_region = eye_region
         self.nose = nose
+
+
+class Masks:
+    def __init__(self,
+                 forehead,
+                 left_eye_region,
+                 right_eye_region,
+                 nose,
+                 lips):
+        self.forehead = forehead
+        self.left_eye_region = left_eye_region
+        self.right_eye_region = right_eye_region
+        self.nose = nose
+        self.lips = lips
+
+    def to_cuda(self):
+        self.forehead = self.forehead.cuda()
+        self.left_eye_region = self.left_eye_region.cuda()
+        self.right_eye_region = self.right_eye_region.cuda()
+        self.nose = self.nose.cuda()
+        self.lips = self.lips.cuda()
 
 
 def upload_face_mask(path=f"{PROJECT_DIR}/FLAME/model/FLAME_masks.pkl"):
@@ -25,10 +46,21 @@ def upload_lips_mask(path=f"{PROJECT_DIR}/FLAME/model/FLAME_masks.pkl"):
     return lips_mask
 
 
+def upload_upper_masks(path=f"{PROJECT_DIR}/FLAME/model/FLAME_masks.pkl"):
+    with open(path, 'rb') as f:
+        masks_dict = pickle.load(f, encoding='latin1')
+        masks = UpperMasks(forehead=masks_dict["forehead"],
+                           eye_region=masks_dict["eye_region"],
+                           nose=masks_dict["nose"])
+    return masks
+
+
 def upload_masks(path=f"{PROJECT_DIR}/FLAME/model/FLAME_masks.pkl"):
     with open(path, 'rb') as f:
         masks_dict = pickle.load(f, encoding='latin1')
-        masks = Masks(forehead=masks_dict["forehead"],
-                      eye_region=masks_dict["eye_region"],
-                      nose=masks_dict["nose"])
+        masks = Masks(forehead=torch.Tensor(masks_dict["forehead"]).type(torch.IntTensor),
+                      left_eye_region=torch.Tensor(masks_dict["left_eye_region"]).type(torch.IntTensor),
+                      right_eye_region=torch.Tensor(masks_dict["right_eye_region"]).type(torch.IntTensor),
+                      nose=torch.Tensor(masks_dict["nose"]).type(torch.IntTensor),
+                      lips=torch.Tensor(masks_dict["lips"]).type(torch.IntTensor))
     return masks
